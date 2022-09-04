da25120bcc9392210e1e20d8987721059d2734b1
Masud Rana <masudrana2779@gmail.com>
2022-09-04 16:11:11 +0600

    tr to done 2.10

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from './rootReducer';
 import myMiddleware from './middleware/myMiddleware';
+import logger from 'redux-logger';
 
 
-const store = createStore(rootReducer,applyMiddleware(myMiddleware));
+const store = createStore(rootReducer,applyMiddleware(logger,myMiddleware));
 
 
 export default store;
