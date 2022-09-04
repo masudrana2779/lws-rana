97cff130cbc82a3e025387071ec87bcc25d1cd15
Masud Rana <masudrana2779@gmail.com>
2022-09-04 16:00:24 +0600

    done 2.10

---
+++ src/redux/middleware/myMiddleware.js
@@ -0,0 +1,10 @@
+// create my first middleware
+const myMiddleware = (stage) => (next) => (action) => {
+  console.log(`state is ${JSON.stringify(stage.getState())}`);
+  console.log(`action is ${JSON.stringify(action)}`);
+
+  //pass action
+  return next(action);
+};
+
+export default myMiddleware;
