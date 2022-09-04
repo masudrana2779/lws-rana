32cf651e8eba8f452aa11b8485972cac265a2e31
Masud Rana <masudrana2779@gmail.com>
2022-09-04 15:50:38 +0600

    done 2.9

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,7 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from './rootReducer';
+import myMiddleware from './middleware/myMiddleware';
 
-// create my first middleware 
-const myMiddleware = (stage) => (next) => (action) => {
-console.log(`state is ${JSON.stringify(stage.getState())}`);
-console.log(`action is ${JSON.stringify(action)}`)
-}
 
 const store = createStore(rootReducer,applyMiddleware(myMiddleware));
 
